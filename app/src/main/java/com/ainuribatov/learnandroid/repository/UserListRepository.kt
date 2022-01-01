7fd9b29cf3c78eb3c4e2a39df0ef05f2c04c1fa9
ainurbl <ainur19dota2@mail.ru>
2022-01-01 22:18:45 +0300

    add person filtering

--- app/src/main/java/com/ainuribatov/learnandroid/repository/UserListRepository.kt
+++ app/src/main/java/com/ainuribatov/learnandroid/repository/UserListRepository.kt
@@ -2,13 +2,14 @@ package com.ainuribatov.learnandroid.repository
 
 import com.ainuribatov.learnandroid.data.network.Api
 import com.ainuribatov.learnandroid.entity.Item
+import com.ainuribatov.learnandroid.entity.UserData
 import com.haroldadmin.cnradapter.NetworkResponse
 import javax.inject.Inject
 
 class UserListRepository @Inject constructor(
     private val api: Api
 ) {
-    suspend fun getUsers(): NetworkResponse<List<Item>, Unit> {
+    suspend fun getUsers(): NetworkResponse<List<UserData>, Unit> {
         return api.getUsers()
     }
 }
